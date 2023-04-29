898860110c8757990e9bd4dcb3b00f3550011080
Josef Bauer <josef.bauer.1st@gmail.com>
2023-04-30 01:09:44 +0200

    Sort deals

--- src/components/user/UserHeader.tsx
+++ src/components/user/UserHeader.tsx
@@ -1,5 +1,4 @@
 import { createResource, For, Suspense } from "solid-js";
-import LoadingSpinner from "~/components/ui/icons/LoadingSpinner";
 import LocationIcon from "~/components/ui/icons/LocationIcon";
 import DealsBadge from "~/components/user/DealsBadge";
 import FavoriteDealerBadge from "~/components/user/FavoriteDealerBadge";
@@ -16,23 +15,23 @@ export default function UserHeader() {
     <div class="flex justify-between text-[#dbdce6]">
       <div class="flex w-full justify-between">
         <div class="m-4 flex flex-col gap-4 pt-2">
-          <div class="flex gap-1.5 fill-current text-[#69828c]">
-            <DealsBadge number={1} />
-            <HotsBadge number={2} />
-            <FavoriteDealerBadge number={3} />
-          </div>
-          <span class="text-2xl">{username()}</span>
-          <span class="flex flex-col gap-2 text-sm">
-            <b>Dein Standort</b>
-            <span class="flex gap-1">
-              <LocationIcon size={1.5} />
-              <div class="flex flex-col text-xs">
-                <Suspense fallback={<LoadingSpinner />}>
+          <Suspense>
+            <div class="flex gap-1.5 fill-current text-[#69828c]">
+              <DealsBadge number={1} />
+              <HotsBadge number={2} />
+              <FavoriteDealerBadge number={3} />
+            </div>
+            <span class="text-2xl">{username()}</span>
+            <span class="flex flex-col gap-2 text-sm">
+              <b>Dein Standort</b>
+              <span class="flex gap-1">
+                <LocationIcon size={1.5} />
+                <div class="flex flex-col text-xs">
                   <For each={address()}>{(addressItem) => <i>{addressItem}</i>}</For>
-                </Suspense>
-              </div>
+                </div>
+              </span>
             </span>
-          </span>
+          </Suspense>
         </div>
         <div class="-mt-6 mr-14 flex flex-col">
           <div class="h-24 w-24">
