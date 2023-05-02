6dfc9c55dc53f0383e4c8eea9a632017755de225
Josef Bauer <josef.bauer.1st@gmail.com>
2023-05-02 17:22:06 +0200

    SSR issues

--- src/components/user/ProfileImage.tsx
+++ src/components/user/ProfileImage.tsx
@@ -1,4 +1,4 @@
-import { createResource, mergeProps, Suspense } from "solid-js";
+import { createSignal, mergeProps, onMount, Suspense } from "solid-js";
 import { menuOpen } from "~/lib/stores/navigation-store";
 import { getProfileImage } from "~/lib/supabase/storage-service";
 
@@ -6,7 +6,9 @@ export default function ProfileImage(props: { id?: string; size?: number; isDeal
   props = mergeProps({ size: 6 }, props);
   const style = `width: ${props.size}rem; height: ${props.size}rem`;
 
-  const [profileImageUrl] = createResource(async () => await getProfileImage({ isDealer: props.isDealer }));
+  const [profileImageUrl, setProfileImageUrl] = createSignal("");
+  onMount(async () => setProfileImageUrl(await getProfileImage({ isDealer: props.isDealer })));
+  // const [profileImageUrl] = createResource(async () => await getProfileImage({ isDealer: props.isDealer }));
 
   return (
     <div class="avatar cursor-pointer" classList={{ invisible: menuOpen() }}>
